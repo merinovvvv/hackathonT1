d65c66a0a814bcada1033645865889748262e190
fndk <pyasecky.code@zohomail.eu>
2024-11-01 12:24:11 +0300

    add parser

--- db_cleaner/src/main/java/by/ww/cleaner/parser/Parser.java
+++ db_cleaner/src/main/java/by/ww/cleaner/parser/Parser.java
@@ -1,9 +1,25 @@
 package by.ww.cleaner.parser;
 
+import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Parser {
-    //
+    Parser(File db_file) {
+        this.db_file = db_file;
+    }
+
+    boolean Connect() {
+        String prefix = "jdbc:sqlite:";
+        try {
+            connection = DriverManager.getConnection(prefix + db_file.getAbsolutePath());
+        } catch (SQLException _) {
+            return false;
+        }
+        return true;
+    }
+
+    Connection connection;
+    File db_file;
 }
